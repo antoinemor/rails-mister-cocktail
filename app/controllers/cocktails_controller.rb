18e463ff8c4df4cfd18ee48a030d14d41f7eddf6
Antoine Moreau <antoine_moreau@me.com>
2016-11-24 12:02:41 -0200

    Finish cocktail controller methods and views

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,9 +9,13 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
