b5ca90ac5a9b786a8d780fe4c97a2aff7e1905bc
Antoine Moreau <antoine_moreau@me.com>
2016-11-24 11:35:13 -0200

    Finish models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,17 @@
 class Ingredient < ApplicationRecord
+  has_many :doses
+
+  validates :name, presence: true, uniqueness: true
+
+  before_destroy :check_for_cocktails
+
+  private
+
+  def check_for_cocktails
+    if cocktails.count > 0
+      errors.add_to_base("cannot delete ingredient when used in cocktail")
+      return false
+    end
+  end
+
 end
